b7b2d47c870e4311d2bde885ea37a77ab2ec6d08
margauuuuuxx <margaux.loncour@gmail.com>
2024-05-21 17:36:43 +0200

    mardi

---
+++ include/fractol.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marlonco <marlonco@students.s19.be>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 10:08:58 by marlonco          #+#    #+#             */
+/*   Updated: 2024/05/15 10:17:59 by marlonco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <fcntl.h> // for open
+# include <unistd.h> // for close, read and write
+# include <stdlib.h> // for malloc and exit
+# include <errno.h> // for errno
+# include <string.h> // for strerror 
+# include <math.h> // maths fcts 
+
+# endif 
