22c7d571d0042bdfb3a58aed014de69ce398c684
margauuuuuxx <margaux.loncour@gmail.com>
2024-08-19 11:53:49 +0200

    h

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: marlonco <marlonco@students.s19.be>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:08:58 by marlonco          #+#    #+#             */
-/*   Updated: 2024/08/18 13:58:52 by marlonco         ###   ########.fr       */
+/*   Updated: 2024/08/18 15:56:24 by marlonco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,44 +34,18 @@
 # define DOWN 125
 # define LEFT 123
 # define RIGHT 124
-# define R 15
-# define C 8
-# define H 4
-# define J 38
-# define P 35
-# define M 46
+
 
 // mouse codes 
 # define SCROLL_UP 4
 # define SCROLL_DOWN 5
-# define MOUSE_CURSOR 6
-
-typedef struct s_mlx	t_mlx; // we define it here because we need t_mlx in t_thread but we need t_thread in t_mlx 
+# define MOUSE_CURSOR 6 
 
 // complex number struct
 typedef struct	s_complex {
 	double	r;
 	double	i;
 }	t_complex;
-
-// viewport struct
-typedef struct	s_viewport {
-	double		xmin;
-	double		xmax;
-	double		ymin;
-	double		ymax;
-	double		zoom;
-	double		offsetx;
-	double		offsety;
-	long		max;
-	t_complex	mouse;
-}	t_viewport;
-
-// pixel struct 
-typedef struct s_pixel {
-	t_complex	c;
-	long		i;
-}	t_pixel;
 
 // image struct (from mlx_get_data_addr())
 typedef struct	s_image {
@@ -91,74 +65,13 @@ typedef struct s_fractal
 	char	*name;
 	double	escape_radius;
 	int		iterations_nbr;
+	double	shift_x;
+	double	shift_y;
+	double	zoom_factor;
+	double	julia_r;
+	double	julia_i;
 }	t_fractal;
 
-// mouse struct
-typedef struct 	s_mouse {
-	char	isdown;
-	int		x;
-	int		y;
-	int		lastx;
-	int		lasty;
-}	t_mouse;
-
-// palette struct
-typedef struct s_palette {
-	uint8_t	count; // since the max value of count is 16, we use this data type, ranging from 0 to 255 for more efficient memory allocation (only use 1 bite VS an int use 4)
-	int		cycle;
-	int		colors[16];
-}	t_palette;
-
-// struct for color gradient 
-typedef struct s_color_RGB {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-} t_color_RGB;
-
-// render struct
-typedef struct s_thread {
-	int		id;
-	t_mlx	*mlx;
-}	t_thread;
-
-typedef struct s_render {
-	pthread_t	threads[THREADS];
-	t_thread	args[THREADS];
-}	t_render;
-
-// mlx struct 
-typedef struct s_mlx {
-
-	void		*mlx;
-	void		*window;
-	t_fractal	*fractal;
-	t_pixel		*data;
-	t_image		*image;
-	t_mouse		mouse;
-	t_viewport	*viewport;
-	t_color_RGB	color;
-	t_render	*render;
-	int			smooth;
-	int			mouselock;
-}	t_mlx;
-
-// struct for color gradient 
-typedef struct s_RGB {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-} t_RGB;
-
-// struct for color
-typedef union u_color { //union is when all the components share the same memory so you can access only 1 value at a time
-	int	value;
-	t_RGB	rgb;
-}	t_color;
-
-// colouring
-void		ft_set_pixel(t_image *image, int x, int y,int color);
-t_color_RGB	ft_color_init(void);
 
 // hook
 int		key_hook(int key_code, t_mlx *mlx);
