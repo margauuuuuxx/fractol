c493ca5f040cd9076658f60dc84e1fec3074bed8
margauuuuuxx <margaux.loncour@gmail.com>
2025-06-12 21:58:19 +0200

    .h complant with linux

--- include/includes.h
+++ include/includes.h
@@ -6,13 +6,20 @@
 /*   By: marlonco <marlonco@students.s19.be>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 19:13:31 by marlonco          #+#    #+#             */
-/*   Updated: 2025/06/12 21:48:32 by marlonco         ###   ########.fr       */
+/*   Updated: 2025/06/12 21:57:18 by marlonco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once 
 
-# include "../minilibx/mlx.h"
+#if defined (__APPLE__)
+    # include "../minilibx/mlx.h"
+#elif defined (__linux__)
+    # include "../minilibx-linux/mlx.h"
+#else 
+    #error "Unsuported platform"
+#endif 
+    
 # include <math.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -22,23 +29,42 @@
 # define WIDTH 800
 # define HEIGHT 400
 
-#define ESC 53
-#define UP 126
-#define DOWN 125
-#define LEFT 123
-#define RIGHT 124
-#define PLUS_MB 24 
-#define MINUS_MB 27
-#define PLUS 69 // on the numeric keypad
-#define MINUS 78
-#define C 8
+#if defined(__APPLE__)
+    #define ESC 53
+    #define UP 126
+    #define DOWN 125
+    #define LEFT 123
+    #define RIGHT 124
+
+    #define PLUS_MB 24    // + on main keyboard
+    #define MINUS_MB 27   // - on main keyboard
+    #define PLUS 69       // + on numeric keypad
+    #define MINUS 78      // - on numeric keypad
+    #define C 8
+
+#elif defined(__linux__)
+    #define ESC 65307
+    #define UP 65362
+    #define DOWN 65364
+    #define LEFT 65361
+    #define RIGHT 65363
+
+    #define PLUS_MB 61   
+    #define MINUS_MB 45   
+    #define PLUS 65451   
+    #define MINUS 65453   
+    #define C 99          
+
+#else
+    #error "Unsupported platform"
+#endif
 
 #define KEY_PRESS 2
 #define KEY_RELEASE 3
-#define SCROLL_UP 4 // zoom in 
-#define SCROLL_DOWN 5 // zoom out
-#define MOUSE_MOVE 6    // MotionNotify
-#define CLOSE_WINDOW 17 // clicking on the x of the window
+#define SCROLL_UP 4
+#define SCROLL_DOWN 5
+#define MOUSE_MOVE 6
+#define CLOSE_WINDOW 17
 
 #define BLACK 0x000000
 #define WHITE 0xFFFFFF
